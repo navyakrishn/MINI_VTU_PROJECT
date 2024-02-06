369a2c1b6278b5ea884240307eae837de080a8ac
Navya DK <dknavya170@gmail.com>
2024-02-06 18:56:24 +0530

    created footer component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import Header from "./Components/Header";
-
+import Footer from "./Components/Footer";
+import {container} from 'react-bootstrap';
 
 function App() {
   return (
   <>
     <Header />
+
+    <container>
+      <main>
+        
+      </main>
+    </container>
+    <Footer/>
   </>
   );
 }
